fff688f6552ac3849a0e7f0306cb53ca06af6e39
xbjzmhml <805700322@qq.com>
2026-02-25 09:18:33 +0800

    Initial Commit

---
+++ shuiguan/shuiguanApp.swift
@@ -0,0 +1,17 @@
+//
+//  shuiguanApp.swift
+//  shuiguan
+//
+//  Created by xblogo on 2026/2/25.
+//
+
+import SwiftUI
+
+@main
+struct shuiguanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
